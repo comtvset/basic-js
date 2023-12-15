a02c83f09babda42b066722bf33dc70a92292641
Vadzim Tsikhaniuk <comtvset@gmail.com>
2023-12-16 00:07:42 +0100

    fix: mine-sweeper (Sat, Dec 16, 2023 12:07:42 AM)

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,91 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(matrix) {
-  const result = matrix.map(subArr => subArr.map(num => num ? 1 : 2));
-  return result;
+function minesweeper(matrix1) {
+    let matrix = matrix1.map((item) => item.map((item2) => (item2 ? 1 : 0)));
+
+    for (let i = 0; i < matrix.length; i += 1) {
+        for (let j = 0; j < matrix[i].length; j += 1) {
+            let currentElement = matrix[i][j];
+
+            if (currentElement !== 1) {
+                if (i - 1 >= 0 && j - 1 >= 0) {
+                    if (matrix[i - 1][j - 1] === 1) {
+                        matrix[i][j] += '1';
+                    }
+                }
+
+                if (i - 1 >= 0) {
+                    if (matrix[i - 1][j] === 1) {
+                        matrix[i][j] += '1';
+                    }
+                }
+
+                if (i - 1 >= 0 && j + 1 < matrix[i].length) {
+                    if (matrix[i - 1][j + 1] === 1) {
+                        matrix[i][j] += '1';
+                    }
+                }
+
+                if (j - 1 >= 0) {
+                    if (matrix[i][j - 1] === 1) {
+                        matrix[i][j] += '1';
+                    }
+                }
+
+                if (j + 1 < matrix[i].length) {
+                    if (matrix[i][j + 1] === 1) {
+                        matrix[i][j] += '1';
+                    }
+                }
+
+                if (i + 1 < matrix.length && j - 1 >= 0) {
+                    if (matrix[i + 1][j - 1] === 1) {
+                        matrix[i][j] += '1';
+                    }
+                }
+
+                if (i + 1 < matrix.length) {
+                    if (matrix[i + 1][j] === 1) {
+                        matrix[i][j] += '1';
+                    }
+                }
+
+                if (i + 1 < matrix.length && j + 1 < matrix[i].length) {
+                    if (matrix[i + 1][j + 1] === 1) {
+                        matrix[i][j] += '1';
+                    }
+                }
+            }
+        }
+    }
+
+    let result = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        let arr = [];
+
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === '011') {
+                arr.push(2);
+            }
+            if (matrix[i][j] === '01') {
+                arr.push(1);
+            }
+            if (matrix[i][j] === 1) {
+                arr.push(1);
+            }
+            if (matrix[i][j] === 0) {
+                arr.push(0);
+            }
+        }
+
+        result.push(arr);
+    }
+
+    return result;
 }
 
 module.exports = {
-  minesweeper
+    minesweeper,
 };
